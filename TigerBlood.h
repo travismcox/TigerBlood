0405d6a37620ea67ba84c93e148264d33059a0d8
travismcox <travis.m.cox@mac.com>
2018-03-26 12:52:13 -0500

    Add files via upload
    Project Setup

---
+++ TigerBlood.h
@@ -0,0 +1,13 @@
+//
+//  TigerBlood.h
+//  Tiger Blood
+//
+//  Created by Travis Cox on 3/26/18.
+//  Copyright © 2018 Travis Cox. All rights reserved.
+//
+
+#ifndef TigerBlood_h
+#define TigerBlood_h
+
+
+#endif /* TigerBlood_h */
---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Tiger Blood
+//
+//  Created by Travis Cox on 3/26/18.
+//  Copyright © 2018 Travis Cox. All rights reserved.
+//
+
+#include <iostream>
+#include "TigerBlood.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
